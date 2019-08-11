cd717d405a6c0cc27347376986e2a5713808b9b3
Christopher X. Shade <6088821+cshade@users.noreply.github.com>
2019-08-10 20:31:43 -0400

    added sort feature, and adjusted Create button size

--- src/components/TaskConsoleDisplay.vue
+++ src/components/TaskConsoleDisplay.vue
@@ -1,8 +1,10 @@
 <template>
 <div class="task-list">
     <h2>Due Tasks</h2>
+
   <ul>
     <li v-show="getTasksForDisplay.length == 0">There are no tasks to display.</li>
+
     <li v-for="task in getTasksForDisplay" :key="task._id">
 
       <toggle-done-button 
@@ -28,8 +30,17 @@
       </task-details-display>
 
     </li>
-  </ul>
 
+    <li><sort-tasks-button
+      v-show="getTasksForDisplay.length > 0"
+      v-on:custom="toggleSort"
+      class="btn btn-primary btn-sm"
+      >toggle sort
+      </sort-tasks-button>
+    </li>
+
+</ul>
+  
   <span class="filter-label">Filter:</span>
 
   <filter-tasks-button
@@ -97,7 +108,7 @@
       
       <li><create-task-button
         v-on:custom="createTask"
-        class="btn btn-primary col-sm-6"
+        class="btn btn-primary col-sm-4"
       >Create
       </create-task-button></li>
     </ul>
@@ -113,7 +124,7 @@ import ToggleShowDetailButton from './ToggleShowDetailButton'
 import TaskDetailsDisplay from './TaskDetailsDisplay'
 import FilterTasksButton from './FilterTasksButton'
 import CreateTaskButton from './CreateTaskButton'
-import BootstrapVue from 'bootstrap-vue'
+import SortTasksButton from './SortTasksButton'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -135,6 +146,8 @@ export default {
       // for the filter button action, triggers a filtered display
       filterKey: 'all',
 
+      sortDirection: true, // asc is default here
+
       // these are for Create Task action
       newTaskName: '',
       newTaskDescr: '',
@@ -149,131 +162,143 @@ export default {
     'task-details-display': TaskDetailsDisplay,
     'filter-tasks-button': FilterTasksButton,
     'create-task-button': CreateTaskButton,
+    'sort-tasks-button': SortTasksButton,
   },
   methods: {
-        // sets the showDetail variable to a given task's id
-        toggleShowDetail(taskId = 0) {
-            (this.showDetail == taskId) ? this.showDetail = 0 : this.showDetail = taskId;
-        },
+      toggleSort() {
+        this.sortDirection = !this.sortDirection;
+        if (this.myTasks) {
+          this.myTasks.sort((a,b) => {
+            if (moment(a.due).isBefore(moment(b.due))) return this.sortDirection?-1:1; 
+            if (moment(a.due).isAfter(moment(b.due))) return this.sortDirection?1:-1;
+            return 0;
+          })
+        }
+      },
 
-        // return overdue state of a given task
-        getOverdue(taskId = 0) {
-          if (taskId != 0) {
-            // find this task in the array
-            let taskToEvaluate = this.myTasks.filter( taskToEvaluate => {
-              return taskToEvaluate._id == taskId; })[0];
+      // sets the showDetail variable to a given task's id
+      toggleShowDetail(taskId = 0) {
+          (this.showDetail == taskId) ? this.showDetail = 0 : this.showDetail = taskId;
+      },
 
-            return moment(taskToEvaluate.due, "YYYY-MM-DD").isBefore(moment(), 'day')
-          } else {
-            return false;
-          }
-        },
-        // return due today state of a given task
-        getDueToday(taskId = 0) {
-          if (taskId != 0) {
-            // find this task in the array
-            let taskToEvaluate = this.myTasks.filter( taskToEvaluate => {
-              return taskToEvaluate._id == taskId; })[0];
-            
-            return moment(taskToEvaluate.due, "YYYY-MM-DD").isSame(moment(), 'day')
-          } else {
-            return false;
-          }
-        },
-        // return due tomorrow state of a given task
-        getDueTomorrow(taskId = 0) {
-          if (taskId != 0) {
-            // find this task in the array
-            let taskToEvaluate = this.myTasks.filter( taskToEvaluate => {
-              return taskToEvaluate._id == taskId; })[0];
-            // create a tomorrow moment object for comparison
-            let tempMoment = moment().add(1, 'days');
-            return moment(taskToEvaluate.due, "YYYY-MM-DD").isSame(tempMoment, 'day')
-          } else {
-            return false;
-          }
-        },
-        // friendly format for display of this task's date
-        displayDate(taskId = 0) {
-          if (taskId != 0) {
-            // find this task in the array
-            let taskToEvaluate = this.myTasks.filter( taskToEvaluate => {
-              return taskToEvaluate._id == taskId; })[0];
-            return (moment(taskToEvaluate.due, "YYYY-MM-DD").isSame(moment(), 'day')) ? 
-              "Today" : moment(taskToEvaluate.due, "YYYY-MM-DD").format("dddd MMMM DD");
-          } else {
-            console.log("displayDate() error: invalid task id");
-            return "Date format error";
-          }
-        },
+      // return overdue state of a given task
+      getOverdue(taskId = 0) {
+        if (taskId != 0) {
+          // find this task in the array
+          let taskToEvaluate = this.myTasks.filter( taskToEvaluate => {
+            return taskToEvaluate._id == taskId; })[0];
 
-        // toggles the "done" state of a given task, via the Task API
-        toggleDone(taskId = 0) {
-          if (taskId != 0) {
-            // find the task in the array
-            let taskToUpdate = this.myTasks.filter( taskToUpdate => {
-              return taskToUpdate._id == taskId; })[0];
-            taskToUpdate.done = !taskToUpdate.done;
+          return moment(taskToEvaluate.due, "YYYY-MM-DD").isBefore(moment(), 'day')
+        } else {
+          return false;
+        }
+      },
+      // return due today state of a given task
+      getDueToday(taskId = 0) {
+        if (taskId != 0) {
+          // find this task in the array
+          let taskToEvaluate = this.myTasks.filter( taskToEvaluate => {
+            return taskToEvaluate._id == taskId; })[0];
+          
+          return moment(taskToEvaluate.due, "YYYY-MM-DD").isSame(moment(), 'day')
+        } else {
+          return false;
+        }
+      },
+      // return due tomorrow state of a given task
+      getDueTomorrow(taskId = 0) {
+        if (taskId != 0) {
+          // find this task in the array
+          let taskToEvaluate = this.myTasks.filter( taskToEvaluate => {
+            return taskToEvaluate._id == taskId; })[0];
+          // create a tomorrow moment object for comparison
+          let tempMoment = moment().add(1, 'days');
+          return moment(taskToEvaluate.due, "YYYY-MM-DD").isSame(tempMoment, 'day')
+        } else {
+          return false;
+        }
+      },
+      // friendly format for display of this task's date
+      displayDate(taskId = 0) {
+        if (taskId != 0) {
+          // find this task in the array
+          let taskToEvaluate = this.myTasks.filter( taskToEvaluate => {
+            return taskToEvaluate._id == taskId; })[0];
+          return (moment(taskToEvaluate.due, "YYYY-MM-DD").isSame(moment(), 'day')) ? 
+            "Today" : moment(taskToEvaluate.due, "YYYY-MM-DD").format("dddd MMMM DD");
+        } else {
+          console.log("displayDate() error: invalid task id");
+          return "Date format error";
+        }
+      },
 
-            const config = {
-              headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
+      // toggles the "done" state of a given task, via the Task API
+      toggleDone(taskId = 0) {
+        if (taskId != 0) {
+          // find the task in the array
+          let taskToUpdate = this.myTasks.filter( taskToUpdate => {
+            return taskToUpdate._id == taskId; })[0];
+          taskToUpdate.done = !taskToUpdate.done;
+
+          const config = {
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+            }
+          };
+          axios.post( 
+              '/update/' + taskId, 
+              taskToUpdate,
+              config)
+            .then( response => this.myTasks = response.data )
+            .catch(function (error) {
+              if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+              } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+              } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
               }
-            };
-            axios.post( 
-                '/update/' + taskId, 
-                taskToUpdate,
-                config)
+              console.log(error.config);
+            });
+        } else {
+          console.log("toggleDone() error: invalid task id");
+        }
+      },
+      deleteTask(taskId) {
+        if (taskId != 0) {
+          axios.delete('/delete/' + taskId)
               .then( response => this.myTasks = response.data )
-              .catch(function (error) {
-                if (error.response) {
-                  // The request was made and the server responded with a status code
-                  // that falls out of the range of 2xx
-                  console.log(error.response.data);
-                  console.log(error.response.status);
-                  console.log(error.response.headers);
-                } else if (error.request) {
-                  // The request was made but no response was received
-                  // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                  // http.ClientRequest in node.js
-                  console.log(error.request);
-                } else {
-                  // Something happened in setting up the request that triggered an Error
-                  console.log('Error', error.message);
-                }
-                console.log(error.config);
-              });
-          } else {
-            console.log("toggleDone() error: invalid task id");
-          }
-        },
-        deleteTask(taskId) {
-          if (taskId != 0) {
-            axios.delete('/delete/' + taskId)
-                .then( response => this.myTasks = response.data )
-              .catch(function (error) {
-                if (error.response) {
-                  // The request was made and the server responded with a status code
-                  // that falls out of the range of 2xx
-                  console.log(error.response.data);
-                  console.log(error.response.status);
-                  console.log(error.response.headers);
-                } else if (error.request) {
-                  // The request was made but no response was received
-                  // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                  // http.ClientRequest in node.js
-                  console.log(error.request);
-                } else {
-                  // Something happened in setting up the request that triggered an Error
-                  console.log('Error', error.message);
-                }
-                console.log(error.config);
-              });
-          } else {
-            console.log("deleteTask() error: invalid task id");
-          }
-        },        
+            .catch(function (error) {
+              if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+              } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+              } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+              }
+              console.log(error.config);
+            });
+        } else {
+          console.log("deleteTask() error: invalid task id");
+        }
+      },        
       // create a new task in the system, via the Task API
       createTask() {
         // clear any pre-existing error condition
@@ -341,7 +366,11 @@ export default {
               let tempMoment = moment().add(1, 'days');
               return this.myTasks.filter(task => moment(task.due, "YYYY-MM-DD").isSame(tempMoment, 'day')).filter(task => !task.done)
           }
-          return this.myTasks.filter(task => !task.done)
+          return this.myTasks.filter(task => !task.done).sort((a,b) => {
+            if (moment(a.due).isBefore(moment(b.due))) return this.sortDirection?-1:1; 
+            if (moment(a.due).isAfter(moment(b.due))) return this.sortDirection?1:-1;
+            return 0;
+          })
         }
         return this.myTasks;
       },
